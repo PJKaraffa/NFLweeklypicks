2be47d6222ba4a27354a7867a6364d53ba1ba488
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-04 16:39:10 -0400

    Refactor supabase client initialization

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,10 @@
 const SUPABASE_URL = "https://vtphufuerrrcqgsprbds.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_wku8_T3jBCqrUK4wZxXdtA_tJQwPh1e";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_wku8_T3jBCqrUK4wZxXdtA_tJQwPh1e";
 
 const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
-
 
