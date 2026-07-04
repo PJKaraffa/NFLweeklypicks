994f29c7cca504c30528837831e1903bcac459ea
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-04 16:56:59 -0400

    Replace SUPABASE_ANON_KEY with SUPABASE_KEY

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,5 @@
 const SUPABASE_URL = "https://vtphufuerrrcqgsprbds.supabase.co";
 
-const SUPABASE_ANON_KEY =
-"sb_publishable_wku8_T3jBCqrUK4wZxXdtA_tJQwPh1e";
+const SUPABASE_KEY = "sb_publishable_wku8_T3jBCqrUK4wZxXdtA_tJQwPh1e";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
-
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
