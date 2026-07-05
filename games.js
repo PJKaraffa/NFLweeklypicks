688ef262e01fefb0f5aae79ad502800dda83d1d8
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-05 11:11:51 -0400

    Add files via upload

---
+++ games.js
@@ -0,0 +1,44 @@
+const NFL_GAMES = {
+  2026: {
+    1: [
+      { id: 1, away: "Dallas", home: "Philadelphia" },
+      { id: 2, away: "Kansas City", home: "Chargers" },
+      { id: 3, away: "Tampa Bay", home: "Atlanta" },
+      { id: 4, away: "Cincinnati", home: "Cleveland" },
+      { id: 5, away: "Miami", home: "Indianapolis" },
+      { id: 6, away: "Las Vegas", home: "New England" },
+      { id: 7, away: "Arizona", home: "New Orleans" },
+      { id: 8, away: "Pittsburgh", home: "Jets" },
+      { id: 9, away: "Giants", home: "Washington" },
+      { id: 10, away: "Carolina", home: "Jacksonville" },
+      { id: 11, away: "Tennessee", home: "Denver" },
+      { id: 12, away: "San Francisco", home: "Seattle" },
+      { id: 13, away: "Detroit", home: "Green Bay" },
+      { id: 14, away: "Houston", home: "Rams" },
+      { id: 15, away: "Baltimore", home: "Buffalo" },
+      { id: 16, away: "Minnesota", home: "Chicago" }
+    ],
+
+    2: [
+      { id: 1, away: "Example Away", home: "Example Home" },
+      { id: 2, away: "Another Away", home: "Another Home" }
+    ],
+
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
+    18: []
+  }
+};
